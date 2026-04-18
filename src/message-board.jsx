2d19137f80be4531e5010963a442d58139059f33
Sophie <snsophie50@gmail.com>
2026-04-18 00:52:11 -0400

    ROUTER FINALLY SET UP GNG

---
+++ src/message-board.jsx
@@ -0,0 +1,10 @@
+function MessageBoard() {
+    return (
+      <div>
+        <h1>Message Board</h1>
+        <p>This is the message board page.</p>
+      </div>
+    );
+  }
+  
+  export default MessageBoard;
---
+++ src/ressources.jsx
@@ -0,0 +1,10 @@
+function Ressources() {
+    return (
+      <div>
+        <h1>Ressources</h1>
+        <p>This is the ressources page.</p>
+      </div>
+    );
+  }
+  
+  export default Ressources;
