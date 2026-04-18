2e923a7cd0a52b33e0e0a35112fa13a2602eaa43
brukar <ec.sheepy@gmail.com>
2026-04-17 22:42:54 -0400

    weee

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import './style.css'
 import heroImg from './assets/hero.png'
 
-document.querySelector().innerHTML = `
-  <ul>
-    <li><a href="#msg">Message</a></li>
-    <li><a href="#cnct">Connect</a></li>
-    <li><a href="#rsrc">Resources</a></li>
-  </ul>
+const app = document.querySelector('#app')
+
+app.innerHTML = `
+
 `
